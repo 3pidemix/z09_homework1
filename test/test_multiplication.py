5dfa42f4863ecc3567126fcdbf407aed7863033f
shubhambalsaraf <65739112+shubhambalsaraf@users.noreply.github.com>
2020-08-16 16:55:37 -0400

    test multiplication update

--- test/test_multiplication.py
+++ test/test_multiplication.py
@@ -1,7 +1,10 @@
 import unittest
 from code.multiplication import multiplication
-class TestMultiplication(unittest.Testcase):
+
+
+class TestMultiplication(unittest.TestCase):
     def test_multiplication(self):
-        self.assertEqual(multiplication(2,4),8)
+        self.assertEqual(multiplication(2, 4), 8)
+
     def test_multiplication_neg(self):
-        self.asserEqual(multiplication(5,4), 18)
+        self.assertEqual(add(2, 5), 11)
