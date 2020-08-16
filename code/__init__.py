66f28784ba724521559ead40fdfc8533a3811b1b
shubhambalsaraf <65739112+shubhambalsaraf@users.noreply.github.com>
2020-08-16 15:49:08 -0400

    subtraction multiplication file creates

--- code/__init__.py
+++ code/__init__.py
@@ -1,7 +1,10 @@
 from .increment import inc
 from .addition import add
-
+from .multiplication import multiplication
+from .subtraction import subtraction
 __all__ = [
     'inc',
-    'add'
+    'add',
+    'multiplication',
+    'subtraction'
 ]
